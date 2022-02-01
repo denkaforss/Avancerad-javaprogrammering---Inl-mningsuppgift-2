b95a1e9c7a50bba967890bf090c534bfca8a7afb
denkaforss <dennis.forss@gmail.com>
2022-02-01 13:27:20 +0100

    1.1 done

--- src/Uppgift1/Main.java
+++ src/Uppgift1/Main.java
@@ -1,6 +1,8 @@
 package Uppgift1;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Objects;
 
 class Person {
     private String name;
@@ -39,18 +41,42 @@ public class Main {
 
     public static void main(String[] args) {
 
+        List<Person> persons = List.of(
+                new Person("Dennis", "Male", 15500),
+                new Person("Hanna", "Female", 28000),
+                new Person("Marianne", "Female", 19000),
+                new Person("Jussi", "Male", 41000),
+                new Person("Bill", "Male", 95000),
+                new Person("Marie", "Female", 34000),
+                new Person("Thomas", "Male", 31000),
+                new Person("Johanna", "Female", 25000),
+                new Person("Mikael", "Male", 39000),
+                new Person("Simon", "Male", 29000)
+        );
+
+        // Upggift 1.1 //
+        DoubleSummaryStatistics doubleSummaryStatistics = new DoubleSummaryStatistics();
+        persons.stream()
+                .filter(str -> Objects.equals(str.getGender(), "Female"))
+                .mapToDouble(Person::getSalary)
+                .forEach(doubleSummaryStatistics);
+        System.out.println("Snittlön för kvinnorna: " + doubleSummaryStatistics.getAverage());
+
+        DoubleSummaryStatistics doubleSummaryStatistics1 = new DoubleSummaryStatistics();
+        persons.stream()
+                .filter(str -> Objects.equals(str.getGender(), "Male"))
+                .mapToDouble(Person::getSalary)
+                .forEach(doubleSummaryStatistics1);
+        System.out.println("Snittlön för männen: " + doubleSummaryStatistics1.getAverage());
+
+        //Upggift 1.2 //
+        DoubleSummaryStatistics doubleSummaryStatistics2 = new DoubleSummaryStatistics();
+        persons.stream()
+                .mapToDouble(Person::getSalary)
+                .forEach(doubleSummaryStatistics2);
+        System.out.println(doubleSummaryStatistics2.getMax());
+
 
     }
-    List<Person> persons = List.of(
-            new Person("Dennis", "Male", 15500),
-            new Person("Hanna", "Female", 28000),
-            new Person("Marianne", "Female", 19000),
-            new Person("Jussi", "Male", 41000),
-            new Person("Bill", "Male", 95000),
-            new Person("Marie", "Female", 34000),
-            new Person("Thomas", "Male", 31000),
-            new Person("Johanna", "Female", 25000),
-            new Person("Mikael", "Male", 39000),
-            new Person("Simon", "Male", 29000)
-    );
+
 }
